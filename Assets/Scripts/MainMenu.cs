25eb056e84e22033b5d8f2d1ef9732c016ebb50c
Shikhar <shikhar.kr007@gmail.com>
2020-02-25 00:33:53 +0530

    Chapter 1- Assignment 2 (Buttons scenes)

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+
+    public void loadScene1 () {
+        SceneManager.LoadScene ("Scene1");
+    }
+    public void loadScene2 () {
+        SceneManager.LoadScene ("Scene2");
+    }
+    public void loadScene3 () {
+        SceneManager.LoadScene ("Scene3");
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
